93b480be613488de27eb7cad8a2e73b6dda5b4b6
“ksigei” <sigeikiprono4@gmail.com>
2022-07-04 23:57:13 +0300

    Home page with bulma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // import Header from './components/Header';
 import Home from './components/Home'
+// import React from 'react';
+// import necessary stuff for links and routing
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';  
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <Home />
+      {/* <Home />   */}
+      <SignUp/>    
     </div>
   );
 }
